7409fe22f28d0e6f2633965e9c43e461e957e42a
Silvan Fuhrer <silvan@auterion.com>
2020-10-07 10:57:49 +0200

    L1: Do only check for wrong tangent_vel if in circle_mode
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/lib/l1/ECL_L1_Pos_Controller.cpp
+++ src/lib/l1/ECL_L1_Pos_Controller.cpp
@@ -265,7 +265,7 @@ ECL_L1_Pos_Controller::navigate_loiter(const Vector2f &vector_A, const Vector2f 
 	float tangent_vel = xtrack_vel_center * loiter_direction;
 
 	/* prevent PD output from turning the wrong way */
-	if (tangent_vel < 0.0f) {
+	if (tangent_vel < 0.0f && _circle_mode) {
 		lateral_accel_sp_circle_pd = math::max(lateral_accel_sp_circle_pd, 0.0f);
 	}
 
